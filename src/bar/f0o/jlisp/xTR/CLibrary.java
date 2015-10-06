299eac9afe31e2c97a3d09c516900b717dd6a005
stockmay <stockmay@f0o.bar>
2015-10-06 14:44:08 +0200

    cleanup-a

---
+++ src/bar/f0o/jlisp/xTR/CLibrary.java
@@ -0,0 +1,38 @@
+/******************************************************************************
+ * Copyright (c) 2015 by                                                      *
+ * Andreas Stockmayer <stockmay@f0o.bar> and                                  *
+ * Mark Schmidt <schmidtm@f0o.bar>                                            *
+ *                                                                            *
+ * This file (ControlMessage.java) is part of JLISP.                          *
+ *                                                                            *
+ * JLISP is free software: you can redistribute it and/or modify              *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * JLISP is distributed in the hope that it will be useful,                   *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with JLISP.  If not, see <http://www.gnu.org/licenses/>.             *
+ ******************************************************************************/
+
+package bar.f0o.jlisp.xTR;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+
+public interface CLibrary extends Library {
+	CLibrary INSTANCE = (CLibrary) Native.loadLibrary((Platform.isWindows() ? "msvcrt" : "c"), CLibrary.class);
+
+	void printf(String format, Object... args);
+	int ioctl(int fd, long request, Object ... args);
+	int open(String path, int oflags);
+	int  read(  int  handle,  byte[] buffer,  int  nbyte );
+	int  write(  int  handle,  byte[] buffer,  int  nbyte  );
+	int  close(int  handle);
+
+}
