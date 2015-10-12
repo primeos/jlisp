66091bff484864c820de60044377fbfbdd4a50af
Mark Schmidt <schmidtm@f0o.bar>
2015-10-12 15:50:14 +0200

    copyright

--- src/bar/f0o/jlisp/xTR/ITRWorker.java
+++ src/bar/f0o/jlisp/xTR/ITRWorker.java
@@ -3,20 +3,20 @@
  * Andreas Stockmayer <stockmay@f0o.bar> and                                  *
  * Mark Schmidt <schmidtm@f0o.bar>                                            *
  *                                                                            *
- * This file (ControlMessage.java) is part of JLISP.                          *
+ * This file (ITRWorker.java) is part of jlisp.                               *
  *                                                                            *
- * JLISP is free software: you can redistribute it and/or modify              *
+ * jlisp is free software: you can redistribute it and/or modify              *
  * it under the terms of the GNU General Public License as published by       *
  * the Free Software Foundation, either version 3 of the License, or          *
  * (at your option) any later version.                                        *
  *                                                                            *
- * JLISP is distributed in the hope that it will be useful,                   *
+ * jlisp is distributed in the hope that it will be useful,                   *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the                *
  * GNU General Public License for more details.                               *
  *                                                                            *
  * You should have received a copy of the GNU General Public License          *
- * along with JLISP.  If not, see <http://www.gnu.org/licenses/>.             *
+ * along with $project.name.If not, see <http://www.gnu.org/licenses/>.       *
  ******************************************************************************/
 
 package bar.f0o.jlisp.xTR;
