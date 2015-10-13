fac1b3af31c08f258437a9b9b20f555028a41fb4
Mark Schmidt <schmidtm@f0o.bar>
2015-10-13 11:45:40 +0200

    read config file+interactive modet

--- src/bar/f0o/jlisp/JLISP.java
+++ src/bar/f0o/jlisp/JLISP.java
@@ -2,19 +2,19 @@
  * Copyright (c) 2015 by                                                      *
  * Andreas Stockmayer <stockmay@f0o.bar> and                                  *
  * Mark Schmidt <schmidtm@f0o.bar>                                            *
- *                                                                            *
+ * *
  * This file (JLISP.java) is part of jlisp.                                   *
- *                                                                            *
+ * *
  * jlisp is free software: you can redistribute it and/or modify              *
  * it under the terms of the GNU General Public License as published by       *
  * the Free Software Foundation, either version 3 of the License, or          *
  * (at your option) any later version.                                        *
- *                                                                            *
+ * *
  * jlisp is distributed in the hope that it will be useful,                   *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the                *
  * GNU General Public License for more details.                               *
- *                                                                            *
+ * *
  * You should have received a copy of the GNU General Public License          *
  * along with $project.name.If not, see <http://www.gnu.org/licenses/>.       *
  ******************************************************************************/
@@ -23,9 +23,11 @@ package bar.f0o.jlisp;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 
 public class JLISP {
@@ -37,19 +39,23 @@ public class JLISP {
     private final static int HAXTR = 7;
     private final static int HARTR = 8;
 
+    private static Properties config;
+
+    public static Properties getConfig() {
+        return JLISP.config;
+    }
+
     public static void main(String[] args) {
-        if(args.length != 1) {
-            System.out.println("usage: java JLISP <config_path>");
+        if (args.length == 0 || args.length > 2) {
+            System.out.println("usage: java JLISP <config_path> [-f]");
             System.exit(-1);
-        }
-        else {
-            Properties prop = new Properties();
-            InputStream in = null;
+        } else {
+            boolean foreground = args.length == 2 ? args[1].equalsIgnoreCase("-f"): false;
+            config = new Properties();
             try {
-                in = new FileInputStream(args[0]);
-                prop.load(in);
-                int component = Integer.parseInt(prop.getProperty("app.component", "1"));
-                switch(component) {
+                config.load(new FileInputStream(args[0]));
+                int component = Integer.parseInt(config.getProperty("app.component", "1"));
+                switch (component) {
                     case JLISP.MS:
                         break;
                     case JLISP.XTR:
@@ -67,6 +73,23 @@ public class JLISP {
                     default:
                         System.err.println("no such component");
                         System.exit(-1);
+                }
+                if (foreground) {
+                    String cmd = "";
+                    Scanner in = new Scanner(System.in);
+                    while (!(cmd = in.nextLine()).equalsIgnoreCase("quit")) {
+                        if (cmd.equalsIgnoreCase("help"))
+                            System.out.printf("Supported commands are:\n\t%s\n\t%s\n\t%s", "show config", "write config", "set <key> <value>");
+                        if (cmd.equalsIgnoreCase("show config")) JLISP.getConfig().list(System.out);
+                        if (cmd.equalsIgnoreCase("write config"))
+                            JLISP.getConfig().store(new FileOutputStream(args[0]), "JLISP config\napp.component: ms=0, xtr=1");
+                        if (cmd.toLowerCase().startsWith("set ")) {
+                            StringTokenizer tk = new StringTokenizer(cmd, " ");
+                            if (tk.countTokens() != 3) break;
+                            tk.nextToken();
+                            JLISP.getConfig().setProperty(tk.nextToken(), tk.nextToken());
+                        }
+                    }
                 }
             } catch (FileNotFoundException e) {
                 System.err.println("config file not found");
